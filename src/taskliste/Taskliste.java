302567319ce8f9abd64c5415a378bfa71007f59f
heinz <bfiwest@schnelleramziel.local>
2017-01-16 13:58:26 +0100

    Version mit Abbruch

--- src/taskliste/Taskliste.java
+++ src/taskliste/Taskliste.java
@@ -20,29 +20,39 @@ public class Taskliste
      */
     public static void main(String[] args)
     {
-        // TODO code application logic here
-        // System.out.println("hello world");
-        System.out.println("Task 1");
-        System.out.println("Task 2");
-        System.out.println("");
-        System.out.println("");
-        System.out.print("geben sie 1 oder 2 (als Zahl) ein!:");
+        int He_eingabe1 = 0;
+        while (true)
+        {
+            // TODO code application logic here
+            // System.out.println("hello world");
+            System.out.println("Task 1");
+            System.out.println("Task 2");
+            System.out.println("");
+            System.out.println("");
+            System.out.println("0 .... Programm beenden");
+            System.out.print("geben sie 1 oder 2 (als Zahl) ein!:");
 
-        try
-        {
-            int He_eingabe1 = new Scanner(System.in).nextInt();
-            if (He_eingabe1 < 0 || He_eingabe1 > 2)
+            try
             {
-                 System.out.println("verbotene Zahl");
-            }             
-            else
-            {
-                System.out.println("Sie haben " + He_eingabe1 + " ausgewählt");
+                He_eingabe1 = new Scanner(System.in).nextInt();
+                if (He_eingabe1 == 0)
+                {
+                    System.out.println("Programm beendet");
+                    return;
+                }
+                if (He_eingabe1 < 1 || He_eingabe1 > 2)
+                {
+                    System.out.println("verbotene Zahl");
+                }
+                else
+                {
+                    System.out.println("Sie haben " + He_eingabe1 + " ausgewählt");
+                }
             }
-        }
-        catch (InputMismatchException error) // Variablen definition 
-        {
-              System.out.println("Dieser Text kann nicht behandetlt werden");
+            catch (InputMismatchException error) // Variablen definition 
+            {
+                System.out.println("Das ist keine Zahl!");
+            }
         }
     }
 
