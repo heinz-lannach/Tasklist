88c33dc215b765332d3c165c9c9c1d0f0403967d
heinz <bfiwest@schnelleramziel.local>
2017-01-16 12:22:05 +0100

    Test 1

--- src/taskliste/Taskliste.java
+++ src/taskliste/Taskliste.java
@@ -11,21 +11,31 @@ import java.util.Scanner;
  *
  * @author bfiwest
  */
-public class Taskliste {
+public class Taskliste
+{
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         // TODO code application logic here
-       // System.out.println("hello world");
-       System.out.println("Task 1");
-       System.out.println("Task 2");
-       System.out.println("");
-       System.out.println("");
-       System.out.print("geben sie 1 oder 2 (als Zahl) ein!:");
-       int He_Test = new Scanner(System.in).nextInt();
-       System.out.println("Sie haben " + He_Test + " ausgewählt");
+        // System.out.println("hello world");
+        System.out.println("Task 1");
+        System.out.println("Task 2");
+        System.out.println("");
+        System.out.println("");
+        System.out.print("geben sie 1 oder 2 (als Zahl) ein!:");
+        int He_eingabe1 = new Scanner(System.in).nextInt();
+        if (He_eingabe1 < 0 || He_eingabe1 > 2)
+        {
+            System.out.println("verbotene Eingabe");
+        }
+        else
+        {
+            System.out.println("Sie haben " + He_eingabe1 + " ausgewählt");
+    
+            }
     }
 
 }
